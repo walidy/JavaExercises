ff8207637edc91db9f089bc169819b66a245d217
walid yusof <walid.yusof@yahoo.co.uk>
2020-04-27 13:30:35 +0100

    hospital: made department method in nurses

--- Hospital/src/Hospital.java
+++ Hospital/src/Hospital.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Hospital  {
-
-
+public class Hospital {
 
 
     List<Patients> depXray = new ArrayList<Patients>();
@@ -12,11 +10,17 @@ public class Hospital  {
     List<Patients> depCheckUp = new ArrayList<Patients>();
 
 
-
-
-
-    public void addPatientToXray(Patients patient){
+    public void addPatientToXray(Patients patient) {
         depXray.add(patient);
+    }
+
+
+    public void addPatientToOperation(Patients patient) {
+        depOperation.add(patient);
+    }
+
+    public void addPatientToCheckUp(Patients patient) {
+        depCheckUp.add(patient);
     }
 
     public List<Patients> getCheckUpList() {
@@ -24,11 +28,4 @@ public class Hospital  {
 
     }
 
-    public void addPatientToOperation(Patients patient){
-        depOperation.add(patient);
-    }
-
-    public void addPatientToCheckUp(Patients patient){
-        depCheckUp.add(patient);
-    }
 }
--- Hospital/src/Main.java
+++ Hospital/src/Main.java
@@ -1,15 +1,16 @@
-public class main {
-
+public class Main {
 
     public static void main(String[] args) {
-
-
         Hospital hospital = new Hospital();
-        Patients patient = new Patients("walid", "yusof", 25, 2456158);
+        Nurses nurses = new Nurses();
+
+        Patients patient = new Patients("adil", "oozeerally", 25, 654848);
         Patients patient2 = new Patients("walid", "yusof", 25, 2456158);
 
         hospital.addPatientToCheckUp(patient2);
+        hospital.addPatientToOperation(patient);
 
         System.out.println(hospital.getCheckUpList().get(0).getFirstName());
+        System.out.println(nurses.getDepartment(hospital, patient.getFirstName()));
     }
 }
